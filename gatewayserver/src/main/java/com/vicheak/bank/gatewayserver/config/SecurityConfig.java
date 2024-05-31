e5687069598b11bdb18b948e076efd7b174268db
Vicheak <suonvicheak991@gmail.com>
2024-05-31 20:57:33 +0700

    update security

---
+++ gatewayserver/src/main/java/com/vicheak/bank/gatewayserver/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.vicheak.bank.gatewayserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import reactor.core.publisher.Mono;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+	
+	@Bean
+	public SecurityWebFilterChain springSecurityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+		serverHttpSecurity.authorizeExchange(exchanges -> exchanges.pathMatchers(HttpMethod.GET).permitAll()
+				.pathMatchers("/vicheakbank/account/**").hasRole("ACCOUNT")
+				.pathMatchers("/vicheakbank/card/**").hasRole("CARD")
+				.pathMatchers("/vicheakbank/loan/**").authenticated())
+				.oauth2ResourceServer(oauth2ResourceServerSpec -> oauth2ResourceServerSpec
+						//.jwt(Customizer.withDefaults()));
+						.jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(grantedAuthorityExtractor())));
+		serverHttpSecurity.csrf(csrfSpec -> csrfSpec.disable()); 
+		return serverHttpSecurity.build(); 
+	}
+	
+	private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthorityExtractor(){
+		
+		JwtAuthenticationConverter authenticationConverter = new JwtAuthenticationConverter();
+		authenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakJwtConverter());
+		
+		return new ReactiveJwtAuthenticationConverterAdapter(authenticationConverter);  
+	}
+
+}
