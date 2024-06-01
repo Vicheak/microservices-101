d1cabc20b2f69c176ea577a4dc937bc71be10942
Vicheak <suonvicheak991@gmail.com>
2024-06-01 23:59:50 +0700

    update project

--- account/src/main/java/com/vicheak/bank/account/service/impl/CustomerServiceImpl.java
+++ account/src/main/java/com/vicheak/bank/account/service/impl/CustomerServiceImpl.java
@@ -2,23 +2,41 @@ package com.vicheak.bank.account.service.impl;
 
 import java.util.List;
 
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Service;
 
+import com.vicheak.bank.account.dto.CustomerMessageDTO;
 import com.vicheak.bank.account.entity.Customer;
 import com.vicheak.bank.account.repository.CustomerRepository;
 import com.vicheak.bank.account.service.CustomerService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CustomerServiceImpl implements CustomerService {
 
 	private final CustomerRepository customerRepository;
+	
+	private final StreamBridge streamBridge; 
 
 	@Override
 	public Customer save(Customer customer) {
-		return customerRepository.save(customer);
+		customer = customerRepository.save(customer);
+		sendCommunication(customer);
+		return customer; 
+	}
+	
+	//this is an output to message broker
+	private void sendCommunication(Customer customer) {
+		CustomerMessageDTO customerMessageDTO = new CustomerMessageDTO(customer.getCustomerId(), 
+				customer.getName(), customer.getEmail(), customer.getMobileNumber());
+		log.info("Sending  communication request for details : {}", customerMessageDTO);
+		//function send() will push message to rabbitMQ via exchange
+		var result = streamBridge.send("sendCommunication-out-0", customerMessageDTO);
+		log.info("Is the communication request successfully triggered? : {}", result);
 	}
 
 	@Override
